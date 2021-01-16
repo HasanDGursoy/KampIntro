fa18cc0e36c91de7dc8a8cda8a1fc53bd9e54074
hasan gürsoy <hasan.d.gursoy@outlook.com.tr>
2021-01-17 00:16:05 +0300

    16 Ocak 3.Ders

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+            Console.WriteLine(sayi1);
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] {100,200,300};
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]); // 999 çıkması beni şaşırttı bro
+            //Yukarının 999 çıkmasının sebebini aşşağıya yazıyorum
+            //int,decimal,float,double,bool = Değer Tipdir
+            //array,class,interface = Referans Tipdir (Sayılar1[0]= 999 olmasının sebibi referans olması)
+
+
+
+
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety -tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias (kategoriEtiketi)
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmismi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+
+        }
+    }
+}
